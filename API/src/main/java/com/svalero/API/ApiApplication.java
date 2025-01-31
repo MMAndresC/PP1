00a3011633c0d0154cf929be87dfe01f2371e4ae
Mar <mandrescasabon@hotmail.com>
2025-01-31 09:50:15 +0100

    :tada: init commit

---
+++ API/src/main/java/com/svalero/API/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
