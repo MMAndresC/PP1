88e9168419ba10335c754914fc589f8f66c9839f
Mar <mandrescasabon@hotmail.com>
2025-01-31 19:55:51 +0100

    :wrench: config files

--- javafx/src/main/java/module-info.java
+++ javafx/src/main/java/module-info.java
@@ -4,7 +4,13 @@ module com.svalero.javafx {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.net.http;
+    requires com.google.gson;
 
     opens com.svalero.javafx to javafx.fxml;
     exports com.svalero.javafx;
+    exports com.svalero.javafx.tasks;
+    opens com.svalero.javafx.tasks to javafx.fxml;
+    exports com.svalero.javafx.controllers;
+    opens com.svalero.javafx.controllers to javafx.fxml;
 }
