bfeee697a76d8327764359dbd930ba66612a44f6
Mar <mandrescasabon@hotmail.com>
2025-02-01 00:20:48 +0100

    :construction: add models

--- javafx/src/main/java/com/svalero/javafx/models/Client.java
+++ javafx/src/main/java/com/svalero/javafx/models/Client.java
@@ -1,6 +1,6 @@
 package com.svalero.javafx.models;
 
-import java.time.LocalDate;
+import java.util.List;
 
 public class Client {
     private long id;
@@ -12,7 +12,7 @@ public class Client {
     private String address;
 
     private int gardenSize; //cubic meters
-    private LocalDate contractEnd;
+    private String contractEnd;
 
     private boolean vip;
 
@@ -20,4 +20,85 @@ public class Client {
 
     private double longitude;
 
+    private List<String> services;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getGardenSize() {
+        return gardenSize;
+    }
+
+    public void setGardenSize(int gardenSize) {
+        this.gardenSize = gardenSize;
+    }
+
+    public String getContractEnd() {
+        return contractEnd;
+    }
+
+    public void setContractEnd(String contractEnd) {
+        this.contractEnd = contractEnd;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public List<String> getServices() {
+        return services;
+    }
+
+    public void setServices(List<String> services) {
+        this.services = services;
+    }
 }
