c42c5be75e3d174e2bfb1598a06206c446877093
Mar <mandrescasabon@hotmail.com>
2025-01-31 12:10:31 +0100

    :sparkles: add logger

--- API/src/main/java/com/svalero/API/controller/ClientController.java
+++ API/src/main/java/com/svalero/API/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.svalero.API.controller;
 
 import com.svalero.API.domain.Client;
 import com.svalero.API.service.ClientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,13 @@ import java.util.List;
 
 @RestController
 public class ClientController {
-
+    private final Logger logger = LoggerFactory.getLogger(ClientController.class);
     @Autowired
     private ClientService clientService;
     @GetMapping("/clients")
     public List<Client> getAll(){
-        return this.clientService.getAll();
+        List<Client> allClients = this.clientService.getAll();
+        this.logger.info("Get all clients");
+        return allClients;
     }
 }
