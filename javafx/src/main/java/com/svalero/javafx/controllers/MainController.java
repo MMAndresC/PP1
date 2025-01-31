2b6f0bef2ba37c102c99af84b95d99a693ce0fa1
Mar <mandrescasabon@hotmail.com>
2025-02-01 00:21:58 +0100

    :sparkles: show entity data in tableview

--- javafx/src/main/java/com/svalero/javafx/controllers/MainController.java
+++ javafx/src/main/java/com/svalero/javafx/controllers/MainController.java
@@ -1,9 +1,10 @@
 package com.svalero.javafx.controllers;
 
+import com.svalero.javafx.tasks.ListTask;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
 
 public class MainController {
@@ -18,10 +19,38 @@ public class MainController {
 
     @FXML
     protected void onSelectList(Event event) {
-        System.out.println(event);
         Button sourceButton = (Button) event.getSource();
-        //if(sourceButton == clientsBtn)
+        String option;
+        if(sourceButton == clientsBtn)
+            option = "clients";
+        else option = "gardeners";
+        ListTask listTask = new ListTask(option, container);
+        controlListTask(listTask);
+        Thread thread = new Thread(listTask);
+        //Close thread if app exit
+        thread.setDaemon(true);
+        thread.start();
+    }
 
+    private void controlListTask(ListTask task){
+        task.setOnSucceeded(event -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Notificacion");
+            alert.setContentText("Listing completed");
+            alert.show();
+        });
+        task.setOnFailed(event -> {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Notificacion");
+            alert.setContentText("Error listing");
+            alert.show();
+        });
+        task.setOnCancelled(event -> {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setHeaderText("Notificacion");
+            alert.setContentText("Listing cancelled");
+            alert.show();
+        });
     }
 
 }
--- javafx/src/main/java/com/svalero/javafx/tasks/ListTask.java
+++ javafx/src/main/java/com/svalero/javafx/tasks/ListTask.java
@@ -3,6 +3,9 @@ package com.svalero.javafx.tasks;
 import com.google.gson.reflect.TypeToken;
 import com.svalero.javafx.models.Client;
 import com.svalero.javafx.models.Gardener;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 
 import java.lang.reflect.Type;
@@ -13,45 +16,50 @@ import java.net.URI;
 import java.util.List;
 
 import com.google.gson.Gson;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class ListTask extends Task<Void> {
     private final String entity;
-    public ListTask(String entity){
+    private final TabPane tabPane;
+
+    public ListTask(String entity, TabPane tab){
         this.entity = entity;
+        this.tabPane = tab;
     }
 
     @Override
     protected Void call() throws Exception{
-        try (HttpClient client = HttpClient.newHttpClient()) {
+        try (HttpClient httpClient = HttpClient.newHttpClient()) {
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create("http://localhost:8080/" + this.entity ))
                     .GET()
                     .build();
-            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+            httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                     .thenApply(HttpResponse::body)
                     .thenAccept(response -> {
                         Gson gson = new Gson();
-                        if(this.entity.equals("clients")){
-                            Type listType = new TypeToken<List<Client>>() {}.getType();
-                            List<Client> clients = gson.fromJson(response, listType);
-                            clients.forEach(System.out::println);
-                        }else if(this.entity.equals("gardeners")){
-                            Type listType = new TypeToken<List<Gardener>>() {}.getType();
-                            List<Client> gardeners = gson.fromJson(response, listType);
-                            gardeners.forEach(System.out::println);
+                        try {
+                            if (this.entity.equals("clients")) {
+                                Type listType = new TypeToken<List<Client>>() {
+                                }.getType();
+                                List<Client> clients = gson.fromJson(response, listType);
+                                createClientTableView(clients);
+                            } else if (this.entity.equals("gardeners")) {
+                                Type listType = new TypeToken<List<Gardener>>() {
+                                }.getType();
+                                List<Gardener> gardeners = gson.fromJson(response, listType);
+                                createGardenerTableView(gardeners);
+                            }
+                        }catch (Exception e) {
+                            System.out.println(e.getMessage());
+                            e.printStackTrace();
                         }
-
-
-
-
-                       /* Platform.runLater(() -> {
-                            label.setText("Título: " + post.getTitle() + "\nCuerpo: " + post.getBody());
-                        });*/
                     })
                     .exceptionally(e -> {
-                     /*   Platform.runLater(() -> {
-                            label.setText("Error: " + e.getMessage());
-                        });*/
                         return null;
                     });
 
@@ -59,6 +67,99 @@ public class ListTask extends Task<Void> {
         return null;
     }
 
+    private void createClientTableView(List<Client> clients){
+        TableView<Client> tableView = new TableView<>();
 
+        TableColumn<Client, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Client, String> phoneColumn = new TableColumn<>("Phone");
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+
+        TableColumn<Client, String> addressColumn = new TableColumn<>("Address");
+        addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+
+        TableColumn<Client, Integer> gardenSizeColumn = new TableColumn<>("Garden Size");
+        gardenSizeColumn.setCellValueFactory(new PropertyValueFactory<>("gardenSize"));
+
+        TableColumn<Client, Boolean> vipColumn = new TableColumn<>("VIP");
+        vipColumn.setCellValueFactory(new PropertyValueFactory<>("vip"));
+
+        TableColumn<Client, Double> latitudeColumn = new TableColumn<>("Latitude");
+        latitudeColumn.setCellValueFactory(new PropertyValueFactory<>("latitude"));
+
+        TableColumn<Client, Double> longitudeColumn = new TableColumn<>("Longitude");
+        longitudeColumn.setCellValueFactory(new PropertyValueFactory<>("longitude"));
+
+        tableView.getColumns().add(nameColumn);
+        tableView.getColumns().add(phoneColumn);
+        tableView.getColumns().add(addressColumn);
+        tableView.getColumns().add(gardenSizeColumn);
+        tableView.getColumns().add(vipColumn);
+        tableView.getColumns().add(latitudeColumn);
+        tableView.getColumns().add(longitudeColumn);
+        ObservableList<Client> observableClients = FXCollections.observableArrayList(clients);
+
+        tableView.setItems(observableClients);
+
+        Tab clientTab = new Tab(this.entity);
+        clientTab.setClosable(true);
+
+        Platform.runLater(() -> {
+            try{
+                clientTab.setContent(tableView);
+                tabPane.getTabs().add(clientTab);
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void createGardenerTableView(List<Gardener> gardeners){
+        TableView<Gardener> tableView = new TableView<>();
+        TableColumn<Gardener, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Gardener, String> phoneColumn = new TableColumn<>("Phone");
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+
+        TableColumn<Gardener, String> addressColumn = new TableColumn<>("Address");
+        addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+
+        TableColumn<Gardener, String> hireDateColumn = new TableColumn<>("Hire Date");
+        hireDateColumn.setCellValueFactory(new PropertyValueFactory<>("hireDate"));
+
+        TableColumn<Gardener, Boolean> selfEmployedColumn = new TableColumn<>("Self Employed");
+        selfEmployedColumn.setCellValueFactory(new PropertyValueFactory<>("selfEmployed"));
+
+        TableColumn<Gardener, String> licensePlateColumn = new TableColumn<>("License Plate");
+        licensePlateColumn.setCellValueFactory(new PropertyValueFactory<>("licensePlate"));
+
+
+        tableView.getColumns().add(nameColumn);
+        tableView.getColumns().add(phoneColumn);
+        tableView.getColumns().add(addressColumn);
+        tableView.getColumns().add(hireDateColumn);
+        tableView.getColumns().add(selfEmployedColumn);
+        tableView.getColumns().add(licensePlateColumn);
+
+        ObservableList<Gardener> observableGardeners = FXCollections.observableArrayList(gardeners);
+
+        tableView.setItems(observableGardeners);
+
+        Tab gardenerTab = new Tab(this.entity);
+        gardenerTab.setClosable(true);
+
+        Platform.runLater(() -> {
+            try{
+                gardenerTab.setContent(tableView);
+                tabPane.getTabs().add(gardenerTab);
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        });
+    }
 
 }
